425c5615819ef1b24a82384356476bb29f8df220
Czarek <czaroosik@gmail.com>
2018-12-30 15:47:46 +0100

    Code refactoring

---
+++ controllers/register.js
@@ -0,0 +1,26 @@
+const handleRegister = (req, res, db, bcrypt) => {
+    const { email, nickname, password } = req.body;
+    const hash = bcrypt.hashSync(password);
+    db.transaction(trx => {
+      trx.insert({hash, email})
+      .into('login')
+      .returning('email')
+      .then(loginEmail => {
+        return trx('users')
+          .returning('*')
+          .insert({
+            email: loginEmail[0],
+            nickname: nickname,
+            joined: new Date()
+          })
+          .then(user => {
+            res.json(user[0]);
+          })
+      })
+      .then(trx.commit)
+      .catch(trx.rollback)
+    })
+    .catch(err => res.status(400).json('Unable to register'))
+  }
+
+module.exports = {handleRegister}
